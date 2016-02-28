67b4d6530cdf8f60c4c38788b4c82734e9efe5f9
Junkuvo <0825elle@gmail.com>
2016-02-28 13:40:59 +0900

    Answers activate

--- app/src/main/java/apps/junkuvo/localimageviewer/MainActivity.java
+++ app/src/main/java/apps/junkuvo/localimageviewer/MainActivity.java
@@ -55,7 +55,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+
+
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(new Crashlytics())
+                .debuggable(true)
+                .build();
+
+        Fabric.with(fabric);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
